4973fb91e3e393be9dac38aeed2ed00ca2e98ab9
vincena2 <vincena2@miamioh.edu>
2015-04-08 13:12:18 -0400

    changes to signup page

---
+++ userInfo.php
@@ -0,0 +1,14 @@
+<?php
+
+$action = $_GET["action"];
+$myText = $_POST["mytext"];
+
+
+if(action = "userInfo.php"){
+	$targetFolder = file:///C:/Users/Angel/Documents/cse201/;
+	file_put_contents($targeFoler."fName", $fName);
+	
+}
+
+
+?>
